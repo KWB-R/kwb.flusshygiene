104532a30923934b3c0e17d0d70119e6cf653669
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-02-22 18:48:25 +0100

    Clean build_new_model()
    - improve spacing and indentation
    - do not use return()

--- R/build_new_model.R
+++ R/build_new_model.R
@@ -12,12 +12,16 @@
 #' @export
 #'
 
-build_new_model <- function(river){
+build_new_model <- function(river)
+{
   paths <- get_paths()
 
-  river_dir <- grep(river, names(paths), value = T)
-  if (length(river_dir) < 1)
+  river_dir <- grep(river, names(paths), value = TRUE)
+
+  if (length(river_dir) < 1) {
+
     return(print(paste0("Could not find directory for ", river, " on server")))
+  }
 
   riverdata <- import_riverdata(paths[[river_dir[3]]])
 
@@ -27,18 +31,27 @@ build_new_model <- function(river){
   print(summary(model))
 
   answer <- readline(prompt = "Do you want to save this model? (y/n): ")
-  if (answer != "y")
-    return(print("No new model created"))
 
-  if(!any(grepl("model", river_dir)))
+  if (answer != "y") {
+
+    return(print("No new model created"))
+  }
+
+  if (! any(grepl("model", river_dir))) {
+
     dir.create(model_dir <- paste0(paths[[river_dir[2]]], "/DATA_model_binary"))
-  else
+
+  } else {
+
     model_dir <- paths[[river_dir[4]]]
+  }
 
   modelcount <- length(dir(model_dir)) + 1
   modelname <- paste0(river, modelcount, "stan")
+
   assign(modelname, model)
+
   save(list = modelname, file = paste0(model_dir, "/", modelname, ".RData"))
 
-  return(print(paste0("A new model has been created: ", modelname)))
+  print(paste0("A new model has been created: ", modelname))
 }
