3b94e778d3e252ca20785393497527bc5b5457d9
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-02-22 18:59:05 +0100

    Clean import_riverdata.R
    - use braces
    - use setNames()

--- R/import_riverdata.R
+++ R/import_riverdata.R
@@ -1,29 +1,37 @@
 # import_riverdata ------------------------------------------------------------
 #'
 #' Read existing River Data
-#' 
-#' Read existing, preprocessed csv files with first column datetime and other 
+#'
+#' Read existing, preprocessed csv files with first column datetime and other
 #' columns variable information.
-#' 
+#'
 #' @param path character-string to a DATA_preprocessed_csv directory
-#' 
+#'
 #' @return Returns a list of data.frames containing the river data
 #' @export
 #' @importFrom readr read_csv
-#' 
+#'
 
-import_riverdata <- function(path){ 
+import_riverdata <- function(path)
+{
   csv_files <- dir(path, "\\.csv", full.names = TRUE)
+
   names(csv_files) <- gsub("\\.csv", "", tolower(basename(csv_files)))
-  
-  line1 <- lapply(csv_files, function(x)
-    scan(x, what = character(), sep = ",", nlines = 1, quiet = T))
-  types <- lapply(line1, function(x)
-    paste0(c("T", rep("d", length(x) - 1)), collapse = ""))
-  
-  result <- lapply(names(csv_files), function(x)
-    readr::read_csv(csv_files[x], col_types = types[[x]]))
-  
-  names(result) <- names(csv_files)
-  return(result)
+
+  line1 <- lapply(csv_files, function(x) {
+
+    scan(x, what = character(), sep = ",", nlines = 1, quiet = T)
+  })
+
+  types <- lapply(line1, function(x) {
+
+    paste0(c("T", rep("d", length(x) - 1)), collapse = "")
+  })
+
+  result <- lapply(names(csv_files), function(x) {
+
+    readr::read_csv(csv_files[x], col_types = types[[x]])
+  })
+
+  stats::setNames(result, names(csv_files))
 }
