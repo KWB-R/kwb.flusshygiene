be24c4d5360a8db37dcdc7567e65dcb2091be2ac
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-02-22 18:18:56 +0100

    Clean assess_bathing_quality_eu.R
    - always use curly braces in if()
    - improve indentation and spacing

--- R/assess_bathing_quality_eu.R
+++ R/assess_bathing_quality_eu.R
@@ -11,31 +11,48 @@
 #' @return Returns a single factor with all quality levels
 #' @export
 
-assess_bathing_quality_eu <- function
-(
-  e.coli,
-  log = TRUE
-)
+assess_bathing_quality_eu <- function(e.coli, log = TRUE)
 {
   e.coli <- stats::na.omit(e.coli)
-  if (length(e.coli) < 16)
+
+  if (length(e.coli) < 16) {
+
     warning("Less than 16 e.coli elements. EU directive does need 16 at least.")
-  if (15 < mean(e.coli))
+  }
+
+  if (15 < mean(e.coli)) {
+
     log <- FALSE
-  if (!log)
+  }
+
+  if (! log) {
+
     e.coli <- log10(e.coli)
+  }
+
   mu <- mean(e.coli)
+
   sigma <- stats::sd(e.coli) # sd uses nominator n-1 which leads to higher sigmas
+
   q90 <- 1.282 * sigma + mu
   q95 <- 1.65  * sigma + mu
 
   levels <- c("excellent", "good", "sufficient", "poor")
 
-  if(q95 < log10(500))
+  if (q95 < log10(500)) {
+
     return(ordered("excellent", levels = levels))
-  if(q95 < log10(1000))
+  }
+
+  if (q95 < log10(1000)) {
+
     return(ordered("good", levels = levels))
-  if(q90 < log10(900))
+  }
+
+  if (q90 < log10(900)) {
+
     return(ordered("sufficient", levels = levels))
+  }
+
   return(ordered("poor", levels = levels))
 }
