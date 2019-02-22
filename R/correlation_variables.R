cf5d7a3ee1b7ca801475c869d9c291143608708e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-02-22 18:54:17 +0100

    Clean correlation_variables.R

--- R/correlation_variables.R
+++ R/correlation_variables.R
@@ -16,25 +16,43 @@
 #' @importFrom tidyr gather
 #'
 
-correlation_scatterplot<- function(df, ...){
+correlation_scatterplot<- function(df, ...)
+{
   cor_values <- correlation_values(df, ...)
 
   df_history <- tidyr::gather(df, "lagday", "n", -1)
-  df_history$lagday <- factor(df_history$lagday, levels = unique(df_history$lagday))
+
+  df_history$lagday <- factor(
+    df_history$lagday, levels = unique(df_history$lagday)
+  )
 
   ylab <- "bacteria (cfu/100ml) (MPN)"
-  if (grepl("^log", names(df)[1])) ylab <- paste(ylab, "- log10")
+
+  if (grepl("^log", names(df)[1])) {
+
+    ylab <- paste(ylab, "- log10")
+  }
+
   variable <- sub("^(log_)?([a-z]{1,3})_.*", "\\2", names(df)[2])
-  xlab <- list(r = "Niederschlag (mm)",
-               q = "Durchfluss (m\u00b3/s)",
-               ka = "Kl\u00e4rwerksabluss (m\u00b3/d)",
-               rad = "Globalstrahlung (W/m\u00b2)")[[variable]]
-  if (grepl("^log", names(df)[2])) xlab <- paste(xlab, "- log10")
+
+  xlabs <-list(
+    r = "Niederschlag (mm)",
+    q = "Durchfluss (m\u00b3/s)",
+    ka = "Kl\u00e4rwerksabluss (m\u00b3/d)",
+    rad = "Globalstrahlung (W/m\u00b2)"
+  )
+
+  xlab <- xlabs[[variable]]
+
+  if (grepl("^log", names(df)[2])) {
+
+    xlab <- paste(xlab, "- log10")
+  }
 
   y <- df_history[,1]
 
   x_max <- max(df_history$n, na.rm = TRUE) * 0.8
-  y_min <- min(df_history[,1], na.rm = TRUE) * 1.25
+  y_min <- min(df_history[, 1], na.rm = TRUE) * 1.25
 
   ggplot2::ggplot(data = df_history, ggplot2::aes_string(x = "n", y = "y")) +
     ggplot2::geom_point() +
@@ -42,7 +60,8 @@ correlation_scatterplot<- function(df, ...){
     ggplot2::geom_label(data = cor_values, ggplot2::aes_string(
       label = "cor_values", x = "x_max", y = "y_min"
     )) +
-    ggplot2::theme_bw() + ggplot2::labs(x = xlab, y = ylab)
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = xlab, y = ylab)
 }
 
 # correlation_values -----------------------------------------------------------
@@ -58,11 +77,15 @@ correlation_scatterplot<- function(df, ...){
 #' \donttest{correlation_values(data.frame(datum = rep("egal",10), e.coli = 1:10, var = 1:10), variable = "var")}
 #'
 
-correlation_values <- function(df, ...){
-  cor_values <- unlist(lapply(df[,-1], function(y){
+correlation_values <- function(df, ...)
+{
+  cor_values <- unlist(lapply(df[,-1], function(y) {
+
     stats::cor(x = df[,1], y = y, use = "pairwise.complete.obs", ...)
-    }))
-  cor_values <- data.frame(cor_values = round(cor_values, 4),
-                           lagday = names(cor_values))
-  return(cor_values)
+  }))
+
+  data.frame(
+    cor_values = round(cor_values, 4),
+    lagday = names(cor_values)
+  )
 }
