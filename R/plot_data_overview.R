df63b9b816abfd4e193ea2fc2bca4fca9504b40d
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-02-22 19:09:01 +0100

    Clean plot_data_overview.R

--- R/plot_data_overview.R
+++ R/plot_data_overview.R
@@ -16,49 +16,58 @@
 #' @importFrom lubridate as_date
 #'
 
-plot_data_overview <- function (riverdata, type = "segment") {
+plot_data_overview <- function (riverdata, type = "segment")
+{
+  stopifnot(type == "segment" || type == "point")
 
-  stopifnot(type == "segment" | type == "point")
-
-  overview <- dplyr::bind_rows(lapply(riverdata, function(df) {
+  overview <- dplyr::bind_rows(.id = "Typ", lapply(riverdata, function(df) {
 
     df <- dplyr::select(df, - dplyr::contains("max"))
+
     df2 <- stats::na.omit(tidyr::gather(df, "site", "n", -1))
+
     df2 <- dplyr::group_by(df2, .data$site)
 
-    if(type == "point")
+    if (type == "point") {
+
       return(df2)
+    }
 
-    return(dplyr::summarise(
-      df2,
-      start = range(.data$datum)[1],
-      end = range(.data$datum)[2])
-    )
+    date_range <- range(df2$datum)
 
-  }), .id = "Typ")
+    return(dplyr::summarise(df2, start = date_range[1], end = date_range[2]))
 
-  p <- ggplot2::ggplot(overview, ggplot2::aes_string(y = "site", color = "Typ")) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1),
-                   text = ggplot2::element_text(size = 15)) +
-    ggplot2::labs(title = "Datenverf\u00fcgbarkeit", x = "Zeitraum",
-         y = "Variable und Standort") +
+  }))
+
+  p <- ggplot2::ggplot(
+    overview, ggplot2::aes_string(y = "site", color = "Typ")
+  ) +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(angle = 60, hjust = 1),
+      text = ggplot2::element_text(size = 15)
+    ) +
+    ggplot2::labs(
+      title = "Datenverf\u00fcgbarkeit", x = "Zeitraum",
+      y = "Variable und Standort"
+    ) +
     ggplot2::scale_x_date(date_breaks = "1 years", date_labels = "%Y") +
     ggplot2::scale_y_discrete(limits = rev(levels(factor(overview$site))))
 
-  if(type == "segment") {
+  if (type == "segment") {
 
     p$data$start <- lubridate::as_date(p$data$start)
     p$data$end <- lubridate::as_date(p$data$end)
 
-    p + ggplot2::geom_segment(size = 4, ggplot2::aes_string(
-      x = "start", xend = "end", yend = "site"
-    ))
+    p + ggplot2::geom_segment(
+      size = 4, ggplot2::aes_string(x = "start", xend = "end", yend = "site")
+    )
 
   } else {
 
     p$data$datum <- lubridate::as_date(p$data$datum)
 
-    p + ggplot2::geom_point(size = 2, alpha = 0.5, ggplot2::aes_string(x = "datum"))
+    p + ggplot2::geom_point(
+      size = 2, alpha = 0.5, ggplot2::aes_string(x = "datum")
+    )
   }
-
 }
