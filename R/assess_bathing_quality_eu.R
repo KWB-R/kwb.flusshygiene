92b05f0d483d8f813b3924a562b69e014c259e06
hsonne <hauke.sonnenberg@gmx.de>
2019-02-23 10:29:13 +0100

    Simplify assess_bathing_quality_eu()
    - swap sides of comparison: "mean > 15" intead of
      "15 < mean"
    - do not repeat "return(ordered(..., levels = levels)"

--- R/assess_bathing_quality_eu.R
+++ R/assess_bathing_quality_eu.R
@@ -16,43 +16,35 @@ assess_bathing_quality_eu <- function(e.coli, log = TRUE)
   e.coli <- stats::na.omit(e.coli)
 
   if (length(e.coli) < 16) {
-
     warning("Less than 16 e.coli elements. EU directive does need 16 at least.")
   }
 
-  if (15 < mean(e.coli)) {
-
+  if (mean(e.coli) > 15) {
     log <- FALSE
   }
 
   if (! log) {
-
     e.coli <- log10(e.coli)
   }
 
+  # sd uses nominator n - 1 which leads to higher sigmas
+  sigma <- stats::sd(e.coli)
   mu <- mean(e.coli)
 
-  sigma <- stats::sd(e.coli) # sd uses nominator n-1 which leads to higher sigmas
-
   q90 <- 1.282 * sigma + mu
-  q95 <- 1.65  * sigma + mu
+  q95 <- 1.650 * sigma + mu
 
-  levels <- c("excellent", "good", "sufficient", "poor")
+  qualities <- c("excellent", "good", "sufficient", "poor")
 
-  if (q95 < log10(500)) {
-
-    return(ordered("excellent", levels = levels))
+  index <- if (q95 < log10(500)) {
+    1
+  } else if (q95 < log10(1000)) {
+    2
+  } else if (q90 < log10(900)) {
+    3
+  } else {
+    4
   }
 
-  if (q95 < log10(1000)) {
-
-    return(ordered("good", levels = levels))
-  }
-
-  if (q90 < log10(900)) {
-
-    return(ordered("sufficient", levels = levels))
-  }
-
-  return(ordered("poor", levels = levels))
+  factor(qualities[index], levels = qualities, ordered = TRUE)
 }
